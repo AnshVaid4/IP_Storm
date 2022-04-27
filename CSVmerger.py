a08542858512741248cdac1f9a2e97a2a155aee1
Ansh Vaid <60707011+AnshVaid4@users.noreply.github.com>
2022-04-27 09:15:50 +0530

    Add files via upload

---
+++ CSVmerger.py
@@ -0,0 +1,26 @@
+#This script is created on Python 3.8.10
+import csv
+
+nf=""
+file =  open("MyFile.txt","a")
+with open('C:\\Users\\Ansh Vaid\\Desktop\\IP_Abuseip_Stats.csv') as fileObjectAB:
+    reader_objAB = csv.reader(fileObjectAB)
+    for ABrow in reader_objAB:
+        flag=0
+        ABip=ABrow[0]
+        #print(ABip)
+        with open('C:\\Users\\Ansh Vaid\\Desktop\\Virustotal_Stats.csv') as fileObjectVT:
+            reader_objVT = csv.reader(fileObjectVT)
+            for VTrow in reader_objVT:
+                VTip=VTrow[0]
+                if VTip==ABip:
+                    ABrow[2]=VTrow[1]
+                    ABrow[3]=VTrow[2]
+                    ABs=(",".join(ABrow))+"\n"
+                    file.write(ABs)
+                    flag=1
+            if flag==0:
+                ABs=(",".join(ABrow))+"\n"
+                file.write(ABs)
+file.close()
+            
